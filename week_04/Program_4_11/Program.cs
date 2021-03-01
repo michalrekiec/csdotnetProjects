524b9042d0f9566c3c35594e43b1013ef5a1c327
Michal Rekiec <mr555@wp.pl>
2021-03-01 18:18:18 +0000

    Add Program_4_11

---
+++ week_04/Program_4_11/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            Console.WriteLine("Podaj liczbę całkowitą: ");
+            num = GetNumber();
+            OddOrEven(num);
+
+            Console.ReadLine();
+        }
+
+        private static void OddOrEven(int number)
+        {
+            if (number % 2 == 0)
+                Console.WriteLine("Liczba jest parzysta");
+            else
+                Console.WriteLine("Liczba jest nieparzysta");
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("To nie jest liczba całkowita");
+                    continue;
+                }
+                return number;
+            }
+        }
+    }
+}
