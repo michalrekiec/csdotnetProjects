45625e8489ef567c3360e0e954488812cd82e4ec
Michal Rekiec <mr555@wp.pl>
2021-03-01 19:36:23 +0000

    Add 'FizzBuzz' project.

---
+++ week_04/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string GetFizzOrBuzz(int number)
+        {
+            if (number % 3 == 0 && number % 5 != 0)
+            {
+                return "Fizz";
+            }    
+            else if (number % 3 != 0 && number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            else if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            else
+            {
+                return Convert.ToString(number);
+            }
+        }
+    }
+}
---
+++ week_04/FizzBuzz/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            int number = 0;
+
+            Console.WriteLine("Podaj liczbę całkowitą. Wpisz 'k' jeśli chcesz wyjść z programu. ");
+
+            while (true)
+            {
+                number = GetValue();
+                Console.WriteLine(fizzBuzz.GetFizzOrBuzz(number));
+            }
+        }
+
+        private static int GetValue()
+        {
+            while(true)
+            {
+                var val = Console.ReadLine();
+                if (val.ToUpper() == "K")
+                {
+                    Environment.Exit(0);
+                }
+
+                if (!int.TryParse(val, out int num))
+                {
+                    Console.WriteLine("To nie jest liczba całkowita");
+                    continue;
+                }
+
+                return num;
+            }
+        }
+    }
+}
