9b0eb035286cbd02a2cc87acc87882dbbfc6d81b
Michal Rekiec <mr555@wp.pl>
2021-03-01 17:40:13 +0000

    Add Program_3_23

---
+++ week_03/Program_3_23/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_3_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int losowa = random.Next(1, 101);
+            int ileProb = 0;
+            int wybor;
+
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Podaj liczbę z zakresu 1-100");
+                    wybor = int.Parse(Console.ReadLine());
+
+                    if (wybor < losowa)
+                        Console.WriteLine("Za mało");
+                    else if (wybor > losowa)
+                        Console.WriteLine("Za dużo");
+
+                    ileProb++;
+                }
+                while (wybor != losowa);
+
+                Console.WriteLine("Brawo!!! Ilość prób: {0}", ileProb);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Zły format wprowadzonych danych.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
